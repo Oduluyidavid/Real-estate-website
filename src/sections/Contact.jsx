af95fe42e41b1d8793e536be70cec062235a6149
unknown <133164451+Oduluyidavid@users.noreply.github.com>
2024-05-26 17:46:01 +0100

    first commit

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDarkMode } from '../components/DarkModeContext'
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 
 const Contact = () => {
+
+  useEffect(() => {
+    Aos.init({
+      offset: 200,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+  }, []);
+
+  const { darkMode, toggledarkMode } = useDarkMode();
+
   return (
-    <div>Contact</div>
+    <div className={`${darkMode ? 'dark bg-black' : 'light bg-transparent'} pb-20`}>
+      <section className={`${darkMode ? 'dark bg-gray-800' : 'light bg-red-100'} lg:w-[95%] w-ful h-fit m-auto 
+      rounded-xl grid lg:grid-cols-2 grid-cols-2 justify-center items-center lg:px-36 px-6 py-20 gap-10`}>
+        <div data-aos='zoom-in' className='bg-white dark:bg-black p-10 flex flex-col 
+         justify-center items-start gap-4 rounded-xl'>
+          <h1 className='text-2xl text-black font-semibold dark:text-white'>Send us a message today</h1>
+          <input type='text' placeholder='Enter your full name here' 
+          className='w-full px-6 py-3 border-2 border-gray-200 rounded-xl' />
+          <input type='email' placeholder='Enter your valid email' 
+          className='w-full px-6 py-3 border-2 border-gray-200 rounded-xl' />
+          <input type='number' placeholder='Enter your vaild mobile number' 
+          className='w-full px-6 py-3 border-2 border-gray-200 rounded-xl' />
+          <textarea name='' id='' cols='30'rows='5' placeholder='Enter your message here...
+          'className='w-full px-6 py-3 border-2 border-gray-200 rounded-xl'></textarea>
+          <button className='bg-red-600 w-full text-md px-8 py-3 text-white font-semibold rounded-xl hover:bg-black dark:hover:bg-red-700 cursor-pointer'>SUBMIT</button>
+        </div>
+        <div className='flex flex-col justify-center items-start gap-8 lg:p-20 py-6'>
+          <h1 data-aos='zoom-in' data-aos-delay='200' className='text-red-500 dark:text-white'>REACH US</h1>
+          <h1 data-aos='zoom-in' data-aos-delay='400' className='text-black text-[40px] font-semibold leading-10 dark:text-white'>
+            There are many alternatives to the standard “Contact Us”. Here are some of them: Talk to Us. Get in Touch.</h1>
+            <button className='bg-red-600 w-full text-md px-8 py-3 text-white font-semibold rounded-xl hover:bg-black dark:hover:bg-red-700 cursor-pointer'>SUBMIT</button>
+        </div>
+      </section>
+
+    </div>
   )
 }
 
