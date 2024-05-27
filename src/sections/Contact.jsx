bd374c1a19080078f035bc961f223d8b67a65944
unknown <133164451+Oduluyidavid@users.noreply.github.com>
2024-05-27 12:32:29 +0100

    first commit

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -22,24 +22,20 @@ const Contact = () => {
       <section className={`${darkMode ? 'dark bg-gray-800' : 'light bg-red-100'} lg:w-[95%] w-ful h-fit m-auto 
       rounded-xl grid lg:grid-cols-2 grid-cols-2 justify-center items-center lg:px-36 px-6 py-20 gap-10`}>
         <div data-aos='zoom-in' className='bg-white dark:bg-black p-10 flex flex-col 
-         justify-center items-start gap-4 rounded-xl'>
+         justify-center items-start gap-4 rounded-xl' >
           <h1 className='text-2xl text-black font-semibold dark:text-white'>Send us a message today</h1>
           <input type='text' placeholder='Enter your full name here' 
-          className='w-full px-6 py-3 border-2 border-gray-200 rounded-xl' />
+          className='w-full px-6 py-3 border-2 border-gray-200 rounded-xl p-5' />
           <input type='email' placeholder='Enter your valid email' 
-          className='w-full px-6 py-3 border-2 border-gray-200 rounded-xl' />
+          className='w-full px-6 py-3 border-2 border-gray-200 rounded-xl p-5' />
           <input type='number' placeholder='Enter your vaild mobile number' 
-          className='w-full px-6 py-3 border-2 border-gray-200 rounded-xl' />
+          className='w-full px-6 py-3 border-2 border-gray-200 rounded-xl p-5' />
           <textarea name='' id='' cols='30'rows='5' placeholder='Enter your message here...
-          'className='w-full px-6 py-3 border-2 border-gray-200 rounded-xl'></textarea>
-          <button className='bg-red-600 w-full text-md px-8 py-3 text-white font-semibold rounded-xl hover:bg-black dark:hover:bg-red-700 cursor-pointer'>SUBMIT</button>
+          'className='w-full px-6 py-3 border-2 border-gray-200 rounded-xl p-5'></textarea>
+          <button className='bg-red-600 w-full text-md px-8 py-3 text-white font-semibold 
+          rounded-xl hover:bg-black dark:hover:bg-red-700 cursor-pointer p-5'>SEND MAIL</button>
         </div>
-        <div className='flex flex-col justify-center items-start gap-8 lg:p-20 py-6'>
-          <h1 data-aos='zoom-in' data-aos-delay='200' className='text-red-500 dark:text-white'>REACH US</h1>
-          <h1 data-aos='zoom-in' data-aos-delay='400' className='text-black text-[40px] font-semibold leading-10 dark:text-white'>
-            There are many alternatives to the standard “Contact Us”. Here are some of them: Talk to Us. Get in Touch.</h1>
-            <button className='bg-red-600 w-full text-md px-8 py-3 text-white font-semibold rounded-xl hover:bg-black dark:hover:bg-red-700 cursor-pointer'>SUBMIT</button>
-        </div>
+       
       </section>
 
     </div>
